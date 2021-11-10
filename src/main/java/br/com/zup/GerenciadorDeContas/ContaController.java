a10a4d20b94fb7d10d5201b13faa27d5bd95eb7b
Raline Silva <ralinesilva1@gmail.com>
2021-11-10 19:12:55 -0300

    Criar classe controller para entrada/saida de dados

---
+++ src/main/java/br/com/zup/GerenciadorDeContas/ContaController.java
@@ -0,0 +1,28 @@
+package br.com.zup.GerenciadorDeContas;
+
+import br.com.zup.GerenciadorDeContas.dtos.ContaEntradaDTO;
+import br.com.zup.GerenciadorDeContas.dtos.ContaSaidaDTO;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/contas")
+public class ContaController {
+    @Autowired
+    private ContaService contaService;
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ContaSaidaDTO cadastrar(@RequestBody ContaEntradaDTO contaEntradaDTO){
+        Conta conta1 = modelMapper.map(contaEntradaDTO, Conta.class);
+        return modelMapper.map(contaService.salvar(conta1), ContaSaidaDTO.class);
+    }
+}
