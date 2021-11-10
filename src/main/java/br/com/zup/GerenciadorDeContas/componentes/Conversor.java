f1b688a1229cef9188a36f0d69610a5c069b8f20
Raline Silva <ralinesilva1@gmail.com>
2021-11-10 16:41:29 -0300

    Criar classe que instancia o ModelMapper

--- src/main/java/br/com/zup/GerenciadorDeContas/componentes/Conversor.java
+++ src/main/java/br/com/zup/GerenciadorDeContas/componentes/Conversor.java
@@ -1,2 +1,12 @@
-package br.com.zup.GerenciadorDeContas.componentes;public class Conversor {
+package br.com.zup.GerenciadorDeContas.componentes;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Conversor {
+
+    public ModelMapper modelMapper (){
+        return new ModelMapper();
+    }
 }
