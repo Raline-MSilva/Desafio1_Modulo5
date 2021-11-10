92a7c11ef3c842140ec669af0bfd9bd7c510634b
Raline Silva <ralinesilva1@gmail.com>
2021-11-10 19:53:17 -0300

    criar metodo mostrar contas cadastradas na controller

--- src/main/java/br/com/zup/GerenciadorDeContas/ContaController.java
+++ src/main/java/br/com/zup/GerenciadorDeContas/ContaController.java
@@ -2,14 +2,19 @@ package br.com.zup.GerenciadorDeContas;
 
 import br.com.zup.GerenciadorDeContas.dtos.ContaEntradaDTO;
 import br.com.zup.GerenciadorDeContas.dtos.ContaSaidaDTO;
+import br.com.zup.GerenciadorDeContas.dtos.ResumoContaDTO;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping ("/contas")
@@ -22,9 +27,17 @@ public class ContaController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ContaSaidaDTO cadastrar(@RequestBody ContaEntradaDTO contaEntradaDTO){
-
         Conta conta1 = modelMapper.map(contaEntradaDTO, Conta.class);
         return modelMapper.map(contaService.salvar(conta1), ContaSaidaDTO.class);
     }
 
+    @GetMapping
+    public List<ResumoContaDTO> mostrarContasCadastradas (){
+        List<ResumoContaDTO> resumoDTO = new ArrayList<>();
+        for (Conta contas : contaService.listarContasCadastradas()){
+            ResumoContaDTO resumoContaDTO = modelMapper.map(contas, ResumoContaDTO.class);
+            resumoDTO.add(resumoContaDTO);
+        }
+        return resumoDTO;
+    }
 }
