7a4288ae014a1b054a35c52b304e3529b5ff8b2b
Raline Silva <ralinesilva1@gmail.com>
2021-11-11 22:05:38 -0300

    adicionar lista para filtrar status

--- src/main/java/br/com/zup/GerenciadorDeContas/ContaRepository.java
+++ src/main/java/br/com/zup/GerenciadorDeContas/ContaRepository.java
@@ -1,8 +1,13 @@
 package br.com.zup.GerenciadorDeContas;
 
+import br.com.zup.GerenciadorDeContas.enums.Status;
+import br.com.zup.GerenciadorDeContas.enums.Tipo;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 
 public interface ContaRepository extends CrudRepository<Conta, Integer> {
+    List<Conta> findAllByStatus(Status status);
 
 }
